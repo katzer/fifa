1d27a5116993656e737be7b6876c154687ffe92e
Sebastián Katzer <katzer.sebastian@googlemail.com>
2019-05-20 20:54:15 +0200

    Build all targets from a single docker image

--- build_config.rb
+++ build_config.rb
@@ -1,6 +1,6 @@
 # Apache 2.0 License
 #
-# Copyright (c) 2016 Sebastian Katzer, appPlant GmbH
+# Copyright (c) 2018 Sebastian Katzer, appPlant GmbH
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -20,16 +20,23 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-def gem_config(conf)
+def gem_config(conf, glibc_version: '2.19')
+  configure_glibc(conf, glibc_version) unless conf.is_a? MRuby::CrossBuild
   conf.gem __dir__
+end
+
+def configure_glibc(conf, version)
+  [conf.cc, conf.cxx].each do |cc|
+    cc.flags << "-include #{ENV['GLIBC_HEADERS']}/x64/force_link_glibc_#{version}.h"
+  end
 end
 
 MRuby::Build.new do |conf|
   toolchain ENV.fetch('TOOLCHAIN', :clang)
 
+  conf.enable_bintest
   conf.enable_debug
   conf.enable_test
-  conf.enable_bintest
 
   gem_config(conf)
 end
@@ -42,6 +49,16 @@ MRuby::Build.new('x86_64-pc-linux-gnu') do |conf|
   end
 
   gem_config(conf)
+end
+
+MRuby::Build.new('x86_64-pc-linux-gnu-glibc-2.9') do |conf|
+  toolchain :clang
+
+  [conf.cc, conf.cxx, conf.linker].each do |cc|
+    cc.flags << '-Oz'
+  end
+
+  gem_config(conf, glibc_version: '2.9')
 end
 
 MRuby::Build.new('x86_64-pc-linux-gnu-openssl') do |conf|
@@ -72,6 +89,7 @@ MRuby::CrossBuild.new('x86_64-apple-darwin15') do |conf|
     cc.command = 'x86_64-apple-darwin15-clang'
     cc.flags  += %w[-Oz -mmacosx-version-min=10.11 -stdlib=libstdc++]
   end
+
   conf.cxx.command      = 'x86_64-apple-darwin15-clang++'
   conf.archiver.command = 'x86_64-apple-darwin15-ar'
 
@@ -88,6 +106,7 @@ MRuby::CrossBuild.new('x86_64-w64-mingw32') do |conf|
     cc.command = 'x86_64-w64-mingw32-gcc'
     cc.flags += %w[-Os -DPCRE_STATIC]
   end
+
   conf.cxx.command      = 'x86_64-w64-mingw32-cpp'
   conf.archiver.command = 'x86_64-w64-mingw32-gcc-ar'
   conf.exts.executable  = '.exe'
